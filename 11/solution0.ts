67a5ce5f4883d004539eb29f8404f3a1b34a013d
Subhasish Biswasray <42065821+biswasray@users.noreply.github.com>
2024-05-02 00:30:02 +0530

    solved: Container With Most Water

---
+++ 11/solution0.ts
@@ -0,0 +1,9 @@
+function maxArea(height: number[]): number {
+    let [a,b,res]=[0,height.length-1,0];
+    while(a<b) {
+        const w= Math.min(height[a],height[b])*(b-a);
+        res=Math.max(res,w);
+        height[a]>height[b]?b--:a++;
+    }
+    return res;
+};
